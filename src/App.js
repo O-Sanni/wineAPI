89396843b92809a254b3f603aebae2ed9460da26
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-11 11:53:18 -0400

    add form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route, Switch, Link , BrowserRouter as Router} from "react-router-dom";
 import WineMenue from "./components/WineMenue";
+import WineForm from "./components/WineForm"
 import './App.css';
 
 class App extends React.Component {
@@ -18,27 +19,30 @@ class App extends React.Component {
            <Link to="/">Main Page</Link>
          </li>
          <li>
-           <Link to="/4871">Chateau De Saint Cosme</Link>
+           <Link to="/4896">Chateau De Saint Cosme</Link>
          </li>
          <li>
-         <Link to="/4872">Lan Rioja Crianza</Link></li>
+         <Link to="/4897">Lan Rioja Crianza</Link></li>
          <li>
-         <Link to="/4873">Margerum Sybarite</Link>
+         <Link to="/4898">Margerum Sybarite</Link>
          </li>
          <li>
-           <Link to="/4874">Owen Roe "Ex Umbris"</Link>
+           <Link to="/4899">Owen Roe "Ex Umbris"</Link>
          </li>
          <li>
-           <Link to="/4875">Rex Hill</Link>
+           <Link to="/4900">Rex Hill</Link>
          </li>
          <li>
-           <Link to="/4876">Viticcio Classico Riserva</Link>
+           <Link to="/4901">Viticcio Classico Riserva</Link>
          </li>
          <li>
-           <Link to="/4877">Chateau Le Doyenne</Link>
+           <Link to="/4902">Chateau Le Doyenne</Link>
          </li>
          <li>
-           <Link to="/4878">Domaine Du Bouscat</Link>
+           <Link to="/4903">Domaine Du Bouscat</Link>
+         </li>
+         <li>
+           <Link to="/add_wine">Add wine</Link>
          </li>
        </ul>
      </nav>
@@ -46,30 +50,33 @@ class App extends React.Component {
        <Route path="/" exact component={"/"}>
          <MainPage />
        </Route>
-       <Route path="/4871" exact component={"/4871"}>
-         <SeventyOne />
+       <Route path="/4896" exact component={"/4896"}>
+         <One />
        </Route>
-       <Route path="/4872" exact component={"/4872"}>
-         <SeventyTwo />
+       <Route path="/4897" exact component={"/4897"}>
+         <Two />
        </Route>
-       <Route path="/4873" exact component={"/4873"}>
-         <SeventyThree />
+       <Route path="/4898" exact component={"/4898"}>
+         <Three />
        </Route>
-       <Route path="/4874" exact component={"/4874"}>
-         <SeventyFour />
+       <Route path="/4899" exact component={"/4899"}>
+         <Four />
        </Route>
-       <Route path="/4875" exact component={"/4875"}>
-         <SeventyFive />
+       <Route path="/4900" exact component={"/4900"}>
+         <Five />
        </Route>
-       <Route path="/4876" exact component={"/4876"}>
-         <SeventySix />
+       <Route path="/4901" exact component={"/4901"}>
+         <Six />
        </Route>
-       <Route path="/4877" exact component={"/4877"}>
-         <SeventySeven />
+       <Route path="/4902" exact component={"/4902"}>
+         <Seven />
        </Route>
-       <Route path="/4878" exact component={"/4878"}>
-         <SeventyEight />
+       <Route path="/4903" exact component={"/4903"}>
+         <Eight />
        </Route>
+       <Router path="/add_wine" exact component={"/add_wine"}>
+         <Form />
+       </Router>
      </Switch>
     </div>
     </Router>
@@ -83,29 +90,33 @@ function MainPage(){
     <h2>Please choose wine</h2>
   </div>)
 }
-function SeventyOne(){
-return <WineMenue id={4871}/> 
+function One(){
+return <WineMenue id={4896}/> 
 }
-function SeventyTwo(){
-return <WineMenue id={4872}/> 
+function Two(){
+return <WineMenue id={4897}/> 
 }
-function SeventyThree(){
-return <WineMenue id={4873}/> 
+function Three(){
+return <WineMenue id={4898}/> 
 }
-function SeventyFour(){
-return <WineMenue id={4874}/> 
+function Four(){
+return <WineMenue id={4899}/> 
 }
-function SeventyFive(){
-return <WineMenue id={4875}/> 
+function Five(){
+return <WineMenue id={4900}/> 
 }
-function SeventySix(){
-return <WineMenue id={4876}/> 
+function Six(){
+return <WineMenue id={4901}/> 
 }
-function SeventySeven(){
-return <WineMenue id={4877}/> 
+function Seven(){
+return <WineMenue id={4902}/> 
 }
-function SeventyEight(){
-return <WineMenue id={4878}/> 
+function Eight(){
+return <WineMenue id={4903}/> 
+}
+
+function Form(){
+  return <WineForm />
 }
 
 export default App;
